d178f63dceb57bd3d535c1932ab2399e12a7276c
15vignesh <109289536+15vignesh@users.noreply.github.com>
2024-12-01 19:01:11 +0530

    Delete myscript.js

--- myscript.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.toggle-button').forEach(function (button) {
-    button.addEventListener('click', function () {
-        var educationItem = this.parentElement;
-        educationItem.classList.toggle('open');
-    });
-});
